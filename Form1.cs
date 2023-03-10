8c1cb41590480e9cade0d2095ea3f73f7b44faa7
fmortega <100325096+fmortega@users.noreply.github.com>
2023-03-09 20:27:24 -0500

    actualizacion

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ensayo
 {
     public partial class Form1 : Form
     {
-        string serviceDetenerIniciar = "Acceso a datos de usuarios_56aad63";
+        string linea;
+            string url1 = "http://rndcws.mintransporte.gov.co:8080/ws/svr008w.dll/soap/IBPMServices";
+            string url2 = "http://rndcws2.mintransporte.gov.co:8080/ws/svr008w.dll/soap/IBPMServices";
+            String ruta = "C:\\Users\\fmoz1\\Desktop\\Simplexity.AsTrans.RNDC.exe.config";
+        String ruta2 = "C:\\Users\\fmoz1\\Desktop\\ejemplo.xml";
+
+        string serviceDetenerIniciar = "Wcmsvc";
         public Form1()
         {
             InitializeComponent();
@@ -22,8 +23,40 @@ namespace ensayo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String SERVICEURL = ConfigurationManager.AppSettings["hola"];
-            MessageBox.Show(SERVICEURL);
+            
+          //  String SERVICEURL = ConfigurationManager.AppSettings["ServicioNombre"];
+          
+            
+         //ManejoXml eliminaNodo= new ManejoXml();
+            //eliminaNodo._DeleteNodo();
+            StreamReader sr = new StreamReader(ruta);
+          
+          
+           linea= sr.ReadLine();
+            while (linea != null) {
+                //MessageBox.Show(linea);
+                Console.WriteLine(linea); 
+                if (linea.Contains("SERVICEURL"))
+
+                {
+
+                    lista.Items.Add(linea);
+
+                }
+
+                linea = sr.ReadLine();
+            }
+            sr.Close();
+            
+            //MessageBox.Show(SERVICEURL);
+            XmlDocument xml = new XmlDocument();
+           
+            
+            xml.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+
+            ConfigurationManager.RefreshSection("appSettings");
+           // MessageBox.Show(SERVICEURL);
+
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -51,7 +84,7 @@ namespace ensayo
 
                     sc.Stop();
                     MessageBox.Show("Servicio denido exitosamente");
-                    
+
                 }
 
                 sc.WaitForStatus(ServiceControllerStatus.Stopped);
@@ -89,7 +122,20 @@ namespace ensayo
 
         private void button3_Click(object sender, EventArgs e)
         {
-            StopService ();
+            StopService();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            XmlWriter xmlWriter = XmlWriter.Create(ruta2);
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("Libros");
+            xmlWriter.WriteStartElement("Libro");
+            xmlWriter.WriteAttributeString("Autor", "Freyder");
+
+            xmlWriter.WriteAttributeString("version", "10000");
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndElement();
         }
     }
 }
---
+++ ManejoXml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ensayo
+{
+    public class ManejoXml
+    {
+        XmlDocument doc;
+        string rutaXml = "C:\\Users\\fmoz1\\Desktop\\Simplexity.AsTrans.RNDC.exe.config";
+        public void _DeleteNodo()
+        {
+            try { 
+            doc.Load(rutaXml);
+
+            XmlNode empleados = doc.DocumentElement;
+
+            XmlNodeList listaEmpleados = doc.SelectNodes("configuration/section");
+
+            foreach (XmlNode item in listaEmpleados)
+            {
+
+                if (item.SelectSingleNode("name").InnerText == "loggingConfiguration")
+                {
+
+                    XmlNode nodoOld = item;
+
+                    empleados.RemoveChild(nodoOld);
+                }
+            }
+
+            doc.Save(rutaXml);
+            }catch(FileNotFoundException ex)
+            {
+                MessageBox.Show(""+ex);
+            }
+        }
+    }
+}
